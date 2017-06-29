c56d72cb9bf170d99898ba79f835995109127c81
Takeya Yuki <yuki@gakkougurashi.net>
2017-06-29 11:22:44 +0800

    Add UI Control Feature

--- app/src/main/java/jp/ruby/rubylibrary/MainActivity.java
+++ app/src/main/java/jp/ruby/rubylibrary/MainActivity.java
@@ -1,12 +1,14 @@
 package jp.ruby.rubylibrary;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.nio.charset.Charset;
 
 import yuki.resource.extended.StorageIOManager;
+import yuki.resource.extended.UIController;
 
 public class MainActivity extends Activity {
 
@@ -16,9 +18,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         TextView tv=(TextView)findViewById(R.id.tv);
         String s= null;
+        UIController.SetStatusBarBackgroundColor(this,Color.BLACK);
+        UIController.SetNavigationBarBackgroundColor(this,Color.RED);
+        //UIController.HideStatusBar(this);
+        UIController.HideNavigationBar(this);
+        //UIController.ShowStatusBar(this);
+        UIController.ShowNavigationBar(this);
         try {
-            StorageIOManager.WriteStringToFileSystem(getApplicationContext(),"/storage/emulated/0/1.txt","TEST IO");
-            s = StorageIOManager.ReadStringFromFileSystem(getApplicationContext(),"/system/bin/sh",true);
+            s=String.format("Screen Size\nReal Size: %d px * %d px\n", UIController.GetRealDisplayWidth(this),UIController.GetRealDisplayHeight(this));
+            s+=String.format("Real Size: %d dp * %d dp\n", UIController.PixelToDP(UIController.GetRealDisplayWidth(this),UIController.GetRealDisplayMetrics(this).density),
+                    UIController.PixelToDP(UIController.GetRealDisplayHeight(this),UIController.GetRealDisplayMetrics(this).density));
+            s+=String.format("Logical Size: %d px * %d px\n", UIController.GetLogicalDisplayWidth(this),UIController.GetLogicalDisplayHeight(this));
+            s+=String.format("Logical Size: %d dp * %d dp\n", UIController.PixelToDP(UIController.GetLogicalDisplayWidth(this),UIController.GetLogicalDisplayMetrics(this).density),
+                    UIController.PixelToDP(UIController.GetLogicalDisplayHeight(this),UIController.GetLogicalDisplayMetrics(this).density));
             tv.setText(s);
         } catch (Exception e) {
             e.printStackTrace();
